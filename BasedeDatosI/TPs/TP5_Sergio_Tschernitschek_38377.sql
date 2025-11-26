e16201a2095ea656917d631110c113f5e54e39c2
Sergio <sergiots1786@gmail.com>
2025-11-25 23:47:21 -0300

    tp5

--- BasedeDatosI/TPs/TP5_Sergio_Tschernitschek_38377.sql
+++ BasedeDatosI/TPs/TP5_Sergio_Tschernitschek_38377.sql
@@ -3,30 +3,30 @@ uno de los cursos con sus notas que hayan promocionado o regularizado la materia
 podrá figurar: P – ‘PROMOCIONADO’, R – ‘REGULAR’).*/
 
 CREATE OR REPLACE VIEW lista_alumnos_inscriptos AS
-SELECT mt.nombre materia,A.apellido || ',' || A.nombre alumno FROM alumno A
-JOIN matricula M ON A.legajo = M.alumno
-JOIN curso C ON M.curso=C.ID
-JOIN materia mt ON mt.ID=C.materia
-WHERE M.promociona = 'P' OR M.promociona ='R'
-ORDER BY mt.nombre,A.apellido;
-
-SELECT * FROM lista_alumnos_inscriptos;
-SELECT DISTINCT materia FROM lista_alumnos_inscriptos;
-SELECT COUNT (DISTINCT materia) FROM lista_alumnos_inscriptos;
-SELECT COUNT (materia) FROM lista_alumnos_inscriptos;
-
-
-
-
-
+SELECT 
+    mt.nombre AS materia,
+    A.apellido || ', ' || A.nombre AS alumno,
+    M.nota1,
+    M.nota2,
+    M.notaf,
+    CASE 
+        WHEN M.promociona = 'P' THEN 'PROMOCIONADO'
+        WHEN M.promociona = 'R' THEN 'REGULAR'
+    END AS condicion
+FROM 
+    alumno A
+    JOIN matricula M ON A.legajo = M.alumno
+    JOIN curso C ON M.curso = C.id
+    JOIN materia mt ON mt.id = C.materia
+WHERE 
+    M.promociona IN ('P', 'R');
 
 
 --2. Modificar el nombre del Campo observaciones de la tabla curso por CONSIDERACIONES.
 
 
-
-
-
+ALTER TABLE curso 
+RENAME COLUMN observaciones TO consideraciones;
 
 
 /*3. Los mails de los profesores se han modificado por la primera letra de su nombre junto a su apellido y su número
@@ -35,33 +35,49 @@ actualización verificar que la cantidad de caracteres mínimos necesarios para 
 estructura de la tabla.*/
 
 
-
-
-
-
+ALTER TABLE profesor MODIFY mail VARCHAR(60);
+UPDATE profesor SET mail = LOWER(substr(nombre,1,1) || apellido || TRIM(to_char(ID)) || '@profesores.frh.edu.ar');
+COMMIT;
 
 /*4. Agregarle un campo a la tabla de profesores para registrar la fecha de alta de cada profesor, que tenga como valor
 base la fecha del día de la inserción.*/
 
 
-
-
-
-
+ ALTER TABLE profesor ADD fecha_alta DATE DEFAULT sysdate;
+ commit;
 
 
 --5. Eliminar los registros de los cursos que no poseen alumnos inscriptos.
 
-
-
-
+ SELECT C.ID FROM curso C 
+ LEFT JOIN matricula M ON M.curso= C.ID
+ WHERE M.curso IS NULL;
+  DELETE FROM curso
+  WHERE ID IN
+            ( SELECT C.ID FROM curso C 
+             LEFT JOIN matricula M ON M.curso= C.ID
+             WHERE M.curso IS NULL);
+ROLLBACK;
 
 
 --6. Listar los datos de los profesores que no están dictando ninguna materia.
 
+select P.id, p.apellido || ',' || p.nombre PROFESOR
+FROM PROFESOR P
+left join curso C on c.profesor=p.id
+where c.profesor is null;
 
 --7. Listar los nombres de las materias que tienen más de tres alumnos inscriptos.
 
+select M.ID,m.nombre  ,   COUNT(MT.ALUMNO)   FROM MATERIA M 
+ JOIN CURSO C on C.MATERIA = M.ID
+ JOIN MATRICULA MT ON MT.CURSO = C.ID
+where MT.ALUMNO NOT LIKE "%organica%"
+  GROUP BY M.ID, M.NOMBRE
+
+ having count (mt.alumno)>3--despues del group by
+ GROUP BY M.NOMBRE;
+ 
 
 /*8. Identificar de los alumnos inscriptos en algún curso sus edades, colocando una leyenda de menor de edad a
   aquellos alumnos con menos de 18 años.   */
