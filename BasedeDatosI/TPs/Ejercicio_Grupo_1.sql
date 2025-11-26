28b2a8c66a201b98c413752ed8dfe48eabd1f0f9
Sergio <sergiots1786@gmail.com>
2025-11-26 14:33:26 -0300

    wallets BD oracle

---
+++ BasedeDatosI/TPs/Ejercicio_Grupo_1.sql
@@ -0,0 +1,45 @@
+/*1. Realizar un listado que muestre por cada detective cuántos son los
+crímenes que investigan en los que aún no tiene ningún sospechoso.*/
+
+
+
+/*
+2. Se han cargado mal las fechas de egreso de todos los criminales que han
+salido de la prisión, en una semana menos de la fecha debida. Ten en
+cuenta que deberás realizar dicha modificación, pero sólo de los presos
+que han salido de prisión.
+*/
+
+
+
+/*3. Eliminar los registros de aquellos criminales que no han cometido delito
+alguno ni son sospechosos de ningún crimen.
+*/
+
+
+
+/*4. Armar un listado de los criminales que no se encuentran en prisión que
+tengan tatuaje, sean altos, sexo masculino y rubios.
+*/
+
+
+/*5. Realizar un listado de los sospechosos a crímenes que se han cometido
+indicando el nombre del criminal, el nombre y apellido del damnificado, el
+crimen cometido y el nombre del detective a cargo de la investigación.
+*/
+
+
+
+/*6. Realizar un listado de criminales que están presos en los que se mostrará
+su nombre, sexo, características físicas, años que llevan preso y una
+leyenda en función de ese tiempo:
+• Si lleva menos de 5 años corresponde que diga 'Sin categorización'.
+• Si lleva entre 5 y 15 años 'Peligrosidad considerable'
+• Si lleva más de más de 15 años corresponde que diga 'Muy peligroso'.
+*/
+
+
+
+
+/*7. Confeccionar una vista que permita tener un informe de los criminales que
+no se encuentran en prisión y son sospechosos de más de un delito.*/
